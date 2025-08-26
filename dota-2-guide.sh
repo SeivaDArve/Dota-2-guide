c375b2ade4a2f281c1a859f6723c28c2745fe4f5
Seiva_Android_OPPO_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-08-27 00:06:32 +0100

    Upg: `dota r web` seleciona um heroi aleatorio e vai imediaeamente buscar um replay desse heroi ao youtube

--- dota-2-guide.sh
+++ dota-2-guide.sh
@@ -2,6 +2,26 @@
 # Title: Dota 2 Guide
 # Description: Interactive menu to co-op during a game
 
+
+
+
+
+
+
+# Sourcing DRYa Lib 1: Color schemes
+   v_lib1=${v_REPOS_CENTER}/DRYa/all/lib/drya-lib-1-colors-greets.sh
+   [[ -f $v_lib1 ]] && source $v_lib1 || (read -s -n 1 -p "DRYa: error: drya-lib-1 does not exist " && echo)
+
+   v_greet="Dota 2"
+   v_talk="Dota: "
+
+   # Examples: `db` (an fx to use during debug)
+   #           f_greet, f_greet2, f_talk, f_done, f_anyK, f_Hline, f_horizlina, f_verticline, etc... [From the repo at: "https://github.com/SeivaDArve/DRYa.git"]
+
+
+
+
+
 clear
 
 function f_title {
@@ -51,6 +71,22 @@ elif [ $1 == "." ]; then
    vim ${v_REPOS_CENTER}/Dota-2-guide/dota-2-guide.sh
 
 elif [ $1 == "r" ]; then
-   bash ${v_REPOS_CENTER}/Dota-2-guide/random-hero-name.sh
+
+   if [ -z $2 ]; then
+      bash ${v_REPOS_CENTER}/Dota-2-guide/random-hero-name.sh
+
+   elif [ $2 == "web" ]; then
+      f_greet
+      f_talk; echo "A abrir no youtube: Gameplay de heroi aleatorio"
+      v_list=${v_REPOS_CENTER}/Dota-2-guide/list-all-dota2-hero-names.txt
+      declare i=$(shuf -i 1-124 -n 1)
+      v_name=$(head -n $i $v_list | tail -n 1)
+      echo " > $v_name"
+      v_search="dota 2 gameplay $v_name"
+      
+      url="https://www.youtube.com/results?search_query=$v_search"
+
+      xdg-open "$url"
+   fi
    
 fi
--- random-hero-name.sh
+++ random-hero-name.sh
@@ -3,10 +3,15 @@
 # Use: Just run the script and get a random hero name
 
 clear
+figlet "Dota 2"
 
-figlet "Dota 2"; echo "Random hero name"
+v_list=${v_REPOS_CENTER}/Dota-2-guide/list-all-dota2-hero-names.txt
+
+echo "Random hero name"
 declare i=$(shuf -i 1-124 -n 1)
 
 echo -ne "Number $i is: "
 
-head -n $i  ${v_REPOS_CENTER}/Dota-2-guide/list-all-dota2-hero-names.txt | tail -n 1
+v_name=$(head -n $i $v_list | tail -n 1)
+
+echo $v_name
