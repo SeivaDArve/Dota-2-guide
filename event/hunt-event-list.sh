3c6d7e55cd73df7b278eccb5063ea78d4373fb7f
Seiva_Android_OPPO_A16s_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-12-03 02:16:03 +0000

    Pushed to github.com automatically (by ezGIT app)

--- event/hunt-event-list.sh
+++ event/hunt-event-list.sh
@@ -7,6 +7,8 @@ function f_create_list {
    v_item=$(cat event-item.txt | fzf)
    v_name=$(cat list-all-dota2-hero-names.txt | fzf -m --prompt="Listar herois: $v_item: ")
 
+   clear
+
    for i in $(echo $v_name)
    do
       v_txt="| $v_item | $i"
@@ -16,18 +18,42 @@ function f_create_list {
 
 function f_filter_list_by_hero {
    v_name=$(cat list-all-dota2-hero-names.txt | fzf -m --prompt="Dota2: Listar os 5 adversarios: $v_item: ")
+
+   clear
+
+   echo "Escolhido (filtro: heroi)"
+   for i in $(echo $v_name)
+   do
+      echo " > $i"
+   done
+   echo
+
+
    for i in $(echo $v_name)
    do
       grep --color="auto" $i $v_list
+      echo
    done
 
 }
 
 function f_filter_list_by_item {
    v_item=$(cat event-item.txt | fzf -m --prompt="Dota2: Listar itens: ")
+
+   clear
+
+   echo "Escolhido (filtro: item)"
+   for i in $(echo $v_item)
+   do
+      echo " > $i"
+   done
+
+   echo
+
    for i in $(echo $v_item)
    do
       grep --color="auto" $i $v_list
+      echo
    done
 
 }
