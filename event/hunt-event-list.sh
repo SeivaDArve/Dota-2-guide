9a471c9ceae1097950501f20a7d106bd8ac30b86
Seiva_ASUS_15_OLED <flowreshe.seiva.d.arve@gmail.com>
2025-12-03 02:07:12 +0000

    Upg: added script for skins event

--- event/hunt-event-list.sh
+++ event/hunt-event-list.sh
@@ -2,11 +2,35 @@
 # Title: fzf chooser for event
 
 v_list=./result-event-list.txt
-v_item=$(cat event-item.txt | fzf)
-v_name=$(cat list-all-dota2-hero-names.txt | fzf -m --prompt="Listar herois: $v_item")
 
-for i in $(echo $v_name)
-do
-   v_txt="| $v_item | $i"
-   echo $v_txt >> $v_list
-done
+function f_create_list {
+   v_item=$(cat event-item.txt | fzf)
+   v_name=$(cat list-all-dota2-hero-names.txt | fzf -m --prompt="Listar herois: $v_item: ")
+
+   for i in $(echo $v_name)
+   do
+      v_txt="| $v_item | $i"
+      echo $v_txt >> $v_list
+   done
+}
+
+function f_filter_list_by_hero {
+   v_name=$(cat list-all-dota2-hero-names.txt | fzf -m --prompt="Dota2: Listar os 5 adversarios: $v_item: ")
+   for i in $(echo $v_name)
+   do
+      grep --color="auto" $i $v_list
+   done
+
+}
+
+function f_filter_list_by_item {
+   v_item=$(cat event-item.txt | fzf -m --prompt="Dota2: Listar itens: ")
+   for i in $(echo $v_item)
+   do
+      grep --color="auto" $i $v_list
+   done
+
+}
+#f_create_list
+#f_filter_list_by_hero
+f_filter_list_by_item
